bd7587dcd8fee9b5dabc737f7c43ec3cc90cd939
GshunS <guoshun.su@gmail.com>
2024-07-16 03:25:15 +1200

    DbContext in dependency injection

--- PersonalBlog/Program.cs
+++ PersonalBlog/Program.cs
@@ -1,3 +1,6 @@
+using PersonalBlog.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var serverVersion = new MySqlServerVersion(new Version(8, 0, 35));
+builder.Services.AddDbContext<BloggingContext>(
+        options => options.UseMySql(builder.Configuration["ConnectionString"], serverVersion));
 
 var app = builder.Build();
 
