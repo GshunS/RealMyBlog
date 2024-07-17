70b43c75c1f8ef8013da6f8724260bb27bbb05f2
GshunS <guoshun.su@gmail.com>
2024-07-18 01:53:27 +1200

    Dependency Injection

--- PersonalBlog/Program.cs
+++ PersonalBlog/Program.cs
@@ -1,5 +1,9 @@
 using PersonalBlog.Models;
 using Microsoft.EntityFrameworkCore;
+using PersonalBlog.Repository.PersonalBlog.IRepository;
+using PersonalBlog.Repository.PersonalBlog.Repository;
+using PersonalBlog.Service.PersonalBlog.IService;
+using PersonalBlog.Service.PersonalBlog.Service;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,18 +14,30 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Dependency Injection
+builder.Services.AddScoped<IAuthorRepository, AuthorRepository>();
+builder.Services.AddScoped<IArticleImageRepository, ArticleImageRepository>();
+builder.Services.AddScoped<IArticleRepository, ArticleRepository>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+
+builder.Services.AddScoped<IAuthorService, AuthorService>();
+builder.Services.AddScoped<IArticleImageService, ArticleImageService>();
+builder.Services.AddScoped<IArticleService, ArticleService>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+
 var serverVersion = new MySqlServerVersion(new Version(8, 0, 35));
 builder.Services.AddDbContext<BloggingContext>(
         options => options.UseMySql(builder.Configuration["ConnectionString"], serverVersion));
 
 var app = builder.Build();
+app.UseSwagger();
+app.UseSwaggerUI();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// // Configure the HTTP request pipeline.
+// if (app.Environment.IsDevelopment())
+// {
+    
+// }
 
 app.UseHttpsRedirection();
 
