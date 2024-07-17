bfe5af20725a2d027c1ca5bb2f37a96825012af9
GshunS <guoshun.su@gmail.com>
2024-07-18 00:27:20 +1200

    implement interfaces

---
+++ PersonalBlog/Repository/PersonalBlog.Repository/ArticleImageRepository.cs
@@ -0,0 +1,8 @@
+using PersonalBlog.Models.Entities;
+using PersonalBlog.Repository.PersonalBlog.IRepository;
+
+namespace PersonalBlog.Repository.PersonalBlog.Repository;
+
+public class ArticleImageRepository: BaseRepository<ArticleImage>, IArticleImageRepository
+{
+}
---
+++ PersonalBlog/Repository/PersonalBlog.Repository/AuthorRepository.cs
@@ -0,0 +1,9 @@
+using PersonalBlog.Models.Entities;
+using PersonalBlog.Repository.PersonalBlog.IRepository;
+
+namespace PersonalBlog.Repository.PersonalBlog.Repository;
+
+public class AuthorRepository: BaseRepository<Author>, IAuthorRepository
+{
+    
+}
