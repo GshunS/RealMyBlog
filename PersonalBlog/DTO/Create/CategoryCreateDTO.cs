fe104b35722cef784b4729f52a0914bb16c18964
GshunS <guoshun.su@gmail.com>
2024-07-19 21:30:49 +1200

    create dto for create method

---
+++ PersonalBlog/DTO/Create/CategoryCreateDTO.cs
@@ -0,0 +1,9 @@
+namespace PersonalBlog.DTO.Create;
+
+public class CategoryCreateDTO
+{
+    public string first_category { get; set; }
+    public string? second_category { get; set; }
+    public string? third_category { get; set; }
+    public string? fourth_category { get; set; }
+}
