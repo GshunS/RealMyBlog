f2a73c8041344c03eb428b8866aea0b00aa98918
GshunS <guoshun.su@gmail.com>
2024-07-29 01:38:55 +1200

    refactor: Update CategoryDisplayDTO and CustomAutoMapperProfile

--- personal_blog_frontend/src/page/BlogContent/Components/NavBar/NavBar.js
+++ personal_blog_frontend/src/page/BlogContent/Components/NavBar/NavBar.js
@@ -7,7 +7,10 @@ import axios from 'axios'
 
 const NavBar = () => {
     const [firstCategoryList, setFirstCategoryList] = useState([])
-
+    // const [secondCategoryObject, setSecondCategoryObject] = useState({})
+    const [secondCategoryName, setSecondCategoryName] = useState({})
+    const [secondCategoryArticle, setSecondCategoryArticle] = useState({})
+    const [expandedCategories, setExpandedCategories] = useState([]);
 
     useEffect(() => {
         async function fetchFirstCategory() {
@@ -15,7 +18,6 @@ const NavBar = () => {
             try {
                 const response = await axios.get(url)
                 setFirstCategoryList(response.data)
-                console.log(response.data)
             } catch (error) {
                 if (error.response) {
                     const status = error.response.status;
@@ -34,6 +36,52 @@ const NavBar = () => {
         fetchFirstCategory()
     }, [])
 
+    const getSecondCategory = async (firstCategory) => {
+        var url = `https://localhost:7219/api/categories/first_category/${firstCategory}`;
+        try {
+            const response = await axios.get(url)
+            console.log(response.data)
+            setSecondCategoryName((preName) => ({
+                ...preName,
+                [firstCategory]: response.data.categoryNames
+            }))
+
+            setSecondCategoryArticle((preArticle) => ({
+                ...preArticle,
+                [firstCategory]: response.data.aritcles
+            }))
+            setExpandedCategories((prevExpanded) => {
+                if (prevExpanded.includes(firstCategory)) {
+                    // If the category is already expanded, collapse it
+                    return prevExpanded.filter((category) => category !== firstCategory);
+                } else {
+                    // If the category is not expanded, expand it
+                    return [...prevExpanded, firstCategory];
+                }
+            });
+            
+        } catch (error) {
+            if (error.response) {
+                const status = error.response.status;
+                if (status === 400) {
+                    console.log([`Bad Request: ${error.response.data}`])
+                } else if (status === 500) {
+                    console.log([`Internal Server Error: ${error.response.data}`])
+                } else {
+                    console.log([`Error: ${error.response.data}`])
+                }
+            } else {
+                console.log([`No response received`])
+            }
+        }
+
+    }
+    
+    // useEffect(()=>{
+    //     console.log(secondCategoryName)
+    //     // console.log(secondCategoryArticle)
+    // }, [secondCategoryName])
+
 
     return (
         <div className="nav-bar">
@@ -45,9 +93,10 @@ const NavBar = () => {
                 <ul className="nav-bar__first-category">
                     {firstCategoryList.map((firstCategory, index) => (
                         <li className="nav-bar__first-category-items"
-                            key = {index}>
+                            key={index}>
                             <div className="nav-bar__category_div">
-                                <div className="nav-bar__category_name">
+                                <div className="nav-bar__category_name"
+                                    onClick={() => getSecondCategory(firstCategory)}>
                                     <img src={angleup} alt="arrow" />
                                     <span>{firstCategory}</span>
 
@@ -58,48 +107,55 @@ const NavBar = () => {
                                 </div>
 
                             </div>
+                            {expandedCategories.includes(firstCategory) && (
+                                <ul className="nav-bar__second-category">
+                                    {
+                                        secondCategoryName[firstCategory].map((secondCategoryName, secondIndex) => (
+                                            <li className="nav-bar__second-category-items"
+                                                key={secondIndex}>
+                                                <div className="nav-bar__category_div">
+                                                    <div className="nav-bar__category_name">
+                                                        <img src={angleup} alt="arrow" />
+                                                        <span>{secondCategoryName}</span>
 
-
-                            {/* <ul className="nav-bar__second-category">
-                                <li className="nav-bar__second-category-items">
-                                    <div className="nav-bar__category_div">
-                                        <div className="nav-bar__category_name">
-                                            <img src={angleup} alt="arrow" />
-                                            <span>{category.second_category}</span>
-
-                                        </div>
-                                        <div className="nav-bar__category_img">
-                                            <img src={addfolder} alt="addfolder" />
-                                            <img src={addfile} alt="addfile" />
-                                        </div>
-
-                                    </div>
-
-                                    <ul className="nav-bar__third-category">
-                                        <li className="nav-bar__third-category-items">
-                                            <div className="nav-bar__category_div">
-                                                <div className="nav-bar__category_name">
-                                                    <img src={angleup} alt="arrow" />
-                                                    <span>{category.third_category}</span>
+                                                    </div>
+                                                    <div className="nav-bar__category_img">
+                                                        <img src={addfolder} alt="addfolder" />
+                                                        <img src={addfile} alt="addfile" />
+                                                    </div>
 
                                                 </div>
-                                                <div className="nav-bar__category_img">
-                                                    <img src={addfolder} alt="addfolder" />
-                                                    <img src={addfile} alt="addfile" />
-                                                </div>
 
-                                            </div>
-                                        </li>
-                                    </ul>
-                                </li>
-                            </ul> */}
+                                                {/* <ul className="nav-bar__third-category">
+                                            <li className="nav-bar__third-category-items">
+                                                <div className="nav-bar__category_div">
+                                                    <div className="nav-bar__category_name">
+                                                        <img src={angleup} alt="arrow" />
+                                                        <span>{category.third_category}</span>
+
+                                                    </div>
+                                                    <div className="nav-bar__category_img">
+                                                        <img src={addfolder} alt="addfolder" />
+                                                        <img src={addfile} alt="addfile" />
+                                                    </div>
+
+                                                </div>
+                                            </li>
+                                        </ul> */}
+                                            </li>
+                                        ))}
+
+                                </ul>
+                            )}
                         </li>
                     ))}
 
                 </ul>
+
             </div>
         </div>
     )
 }
+
 
 export default NavBar
