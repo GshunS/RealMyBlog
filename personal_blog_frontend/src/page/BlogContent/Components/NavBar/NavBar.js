5d70dc3ddee9f4cb46d9746ccb03fcf5bd94700d
GshunS <guoshun.su@gmail.com>
2024-07-29 06:25:46 +1200

    refactor: Update NavBar component CSS styles and optimize code

--- personal_blog_frontend/src/page/BlogContent/Components/NavBar/NavBar.js
+++ personal_blog_frontend/src/page/BlogContent/Components/NavBar/NavBar.js
@@ -50,7 +50,7 @@ const NavBar = () => {
 
             setSecondCategoryArticle((preArticle) => ({
                 ...preArticle,
-                [firstCategory]: response.data.aritcles
+                [firstCategory]: response.data.articles
             }))
             setExpandedCategories((prevExpanded) => {
                 if (prevExpanded.includes(firstCategory)) {
@@ -78,12 +78,6 @@ const NavBar = () => {
         }
 
     }
-
-    // useEffect(()=>{
-    //     console.log(secondCategoryName)
-    //     // console.log(secondCategoryArticle)
-    // }, [secondCategoryName])
-
 
     return (
         <div className="nav-bar">
@@ -119,8 +113,8 @@ const NavBar = () => {
 
                                 </div>
                                 <div className="nav-bar__category_img">
-                                    <img src={addfolder} alt="addfolder" />
-                                    <img src={addfile} alt="addfile" />
+                                    <img src={addfolder} alt="addfolder" title='create a folder' />
+                                    <img src={addfile} alt="addfile" title='create a file' />
                                 </div>
 
                             </div>
@@ -145,33 +139,59 @@ const NavBar = () => {
 
                                                 </div>
                                                 <div className="nav-bar__category_img">
-                                                    <img src={addfolder} alt="addfolder" />
-                                                    <img src={addfile} alt="addfile" />
+                                                    <img src={addfolder} alt="addfolder" title='create a folder' />
+                                                    <img src={addfile} alt="addfile" title='create a file' />
                                                 </div>
 
                                             </div>
 
                                             {/* <ul className="nav-bar__third-category">
-                                        <li className="nav-bar__third-category-items">
-                                            <div className="nav-bar__category_div">
-                                                <div className="nav-bar__category_name">
-                                                    <img src={angleup} alt="arrow" />
-                                                    <span>{category.third_category}</span>
+                                                <li className="nav-bar__third-category-items">
+                                                    <div className="nav-bar__category_div">
+                                                        <div className="nav-bar__category_name">
+                                                            <img src={angleup} alt="arrow" />
+                                                            <span>{category.third_category}</span>
 
-                                                </div>
-                                                <div className="nav-bar__category_img">
-                                                    <img src={addfolder} alt="addfolder" />
-                                                    <img src={addfile} alt="addfile" />
-                                                </div>
+                                                        </div>
+                                                        <div className="nav-bar__category_img">
+                                                            <img src={addfolder} alt="addfolder" />
+                                                            <img src={addfile} alt="addfile" />
+                                                        </div>
 
-                                            </div>
-                                        </li>
-                                    </ul> */}
+                                                    </div>
+                                                </li>
+                                            </ul> */}
                                         </li>
                                     ))
 
+                                )}
+                                <div className={classNames({'nav-bar__category_article': expandedCategories.includes(firstKey)})}>
+                                {expandedCategories.includes(firstKey) && (
+
+                                    secondCategoryArticle[firstKey].map((item, secondIndex) => (
+
+                                        <li className={classNames("nav-bar__second-category-items")}
+                                            key={secondIndex}>
+                                            <div className="nav-bar__category_div">
+                                                <div className="nav-bar__category_name">
+
+                                                    <img
+                                                        src={angleup}
+                                                        alt="arrow"
+                                                        className={classNames(
+                                                            { hideFileArrow: true },
+                                                            { imgRotate: false }
+                                                        )} />
+                                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M64 0C28.7 0 0 28.7 0 64L0 448c0 35.3 28.7 64 64 64l256 0c35.3 0 64-28.7 64-64l0-288-128 0c-17.7 0-32-14.3-32-32L224 0 64 0zM256 0l0 128 128 0L256 0zM112 256l160 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-160 0c-8.8 0-16-7.2-16-16s7.2-16 16-16zm0 64l160 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-160 0c-8.8 0-16-7.2-16-16s7.2-16 16-16zm0 64l160 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-160 0c-8.8 0-16-7.2-16-16s7.2-16 16-16z" /></svg>
+                                                    <span>{item.title}</span>
+
+                                                </div>
+                                            </div>
+                                        </li>
+                                    ))
 
                                 )}
+                                </div>
                             </ul>
                         </li>
                     ))}
