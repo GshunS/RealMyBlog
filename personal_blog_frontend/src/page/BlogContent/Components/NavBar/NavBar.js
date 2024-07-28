764a985c23a35d0996e72ec08fa367caa94a39bd
GshunS <guoshun.su@gmail.com>
2024-07-29 06:29:45 +1200

    refactor: Update NavBar component to use categoryDict for second category names

--- personal_blog_frontend/src/page/BlogContent/Components/NavBar/NavBar.js
+++ personal_blog_frontend/src/page/BlogContent/Components/NavBar/NavBar.js
@@ -134,7 +134,9 @@ const NavBar = () => {
                                                             { imgRotate: false }
                                                         )} />
                                                     {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path fill="#74C0FC" d="M64 0C28.7 0 0 28.7 0 64L0 448c0 35.3 28.7 64 64 64l256 0c35.3 0 64-28.7 64-64l0-288-128 0c-17.7 0-32-14.3-32-32L224 0 64 0zM256 0l0 128 128 0L256 0zM112 256l160 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-160 0c-8.8 0-16-7.2-16-16s7.2-16 16-16zm0 64l160 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-160 0c-8.8 0-16-7.2-16-16s7.2-16 16-16zm0 64l160 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-160 0c-8.8 0-16-7.2-16-16s7.2-16 16-16z"/></svg> */}
-                                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M64 480H448c35.3 0 64-28.7 64-64V160c0-35.3-28.7-64-64-64H288c-10.1 0-19.6-4.7-25.6-12.8L243.2 57.6C231.1 41.5 212.1 32 192 32H64C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64z" /></svg>
+                                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                                                        <path d="M64 480H448c35.3 0 64-28.7 64-64V160c0-35.3-28.7-64-64-64H288c-10.1 0-19.6-4.7-25.6-12.8L243.2 57.6C231.1 41.5 212.1 32 192 32H64C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64z" />
+                                                    </svg>
                                                     <span>{secondCategoryName}</span>
 
                                                 </div>
@@ -165,32 +167,34 @@ const NavBar = () => {
                                     ))
 
                                 )}
-                                <div className={classNames({'nav-bar__category_article': expandedCategories.includes(firstKey)})}>
-                                {expandedCategories.includes(firstKey) && (
+                                <div className={classNames({ 'nav-bar__category_article': expandedCategories.includes(firstKey) })}>
+                                    {expandedCategories.includes(firstKey) && (
 
-                                    secondCategoryArticle[firstKey].map((item, secondIndex) => (
+                                        secondCategoryArticle[firstKey].map((item, secondIndex) => (
 
-                                        <li className={classNames("nav-bar__second-category-items")}
-                                            key={secondIndex}>
-                                            <div className="nav-bar__category_div">
-                                                <div className="nav-bar__category_name">
+                                            <li className={classNames("nav-bar__second-category-items")}
+                                                key={secondIndex}>
+                                                <div className="nav-bar__category_div">
+                                                    <div className="nav-bar__category_name">
 
-                                                    <img
-                                                        src={angleup}
-                                                        alt="arrow"
-                                                        className={classNames(
-                                                            { hideFileArrow: true },
-                                                            { imgRotate: false }
-                                                        )} />
-                                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M64 0C28.7 0 0 28.7 0 64L0 448c0 35.3 28.7 64 64 64l256 0c35.3 0 64-28.7 64-64l0-288-128 0c-17.7 0-32-14.3-32-32L224 0 64 0zM256 0l0 128 128 0L256 0zM112 256l160 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-160 0c-8.8 0-16-7.2-16-16s7.2-16 16-16zm0 64l160 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-160 0c-8.8 0-16-7.2-16-16s7.2-16 16-16zm0 64l160 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-160 0c-8.8 0-16-7.2-16-16s7.2-16 16-16z" /></svg>
-                                                    <span>{item.title}</span>
+                                                        <img
+                                                            src={angleup}
+                                                            alt="arrow"
+                                                            className={classNames(
+                                                                { hideFileArrow: true },
+                                                                { imgRotate: false }
+                                                            )} />
+                                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+                                                            <path d="M64 0C28.7 0 0 28.7 0 64L0 448c0 35.3 28.7 64 64 64l256 0c35.3 0 64-28.7 64-64l0-288-128 0c-17.7 0-32-14.3-32-32L224 0 64 0zM256 0l0 128 128 0L256 0zM112 256l160 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-160 0c-8.8 0-16-7.2-16-16s7.2-16 16-16zm0 64l160 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-160 0c-8.8 0-16-7.2-16-16s7.2-16 16-16zm0 64l160 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-160 0c-8.8 0-16-7.2-16-16s7.2-16 16-16z" />
+                                                        </svg>
+                                                        <span>{item.title}</span>
 
+                                                    </div>
                                                 </div>
-                                            </div>
-                                        </li>
-                                    ))
+                                            </li>
+                                        ))
 
-                                )}
+                                    )}
                                 </div>
                             </ul>
                         </li>
