4460596f4c1ae40930ff8a5c7840a99db0767173
GshunS <guoshun.su@gmail.com>
2024-08-03 23:01:48 +1200

    refactor: Update NavBar component to include folder and file icons

--- personal_blog_frontend/src/page/BlogContent/Components/NavBar/NavBar.js
+++ personal_blog_frontend/src/page/BlogContent/Components/NavBar/NavBar.js
@@ -3,6 +3,7 @@ import './NavBar.css'
 import { useEffect, useState, useRef } from 'react'
 import classNames from 'classnames'
 import axios from 'axios'
+import NavBarElementOperation from './NavBarElementOperations'
 
 // category navigation bar
 const NavBar = () => {
@@ -51,7 +52,7 @@ const NavBar = () => {
         }, 100)
 
         return () => clearTimeout(timer)
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [expandedCategories])
 
     // fetch the first category
@@ -84,7 +85,7 @@ const NavBar = () => {
         let prevExpanded = { ...expandedCategories }
         let currentLevel = null
         let currentExpanded = null
-        let currentCategory = { ...allCategories}
+        let currentCategory = { ...allCategories }
         if (firstCategory) {
             currentLevel = firstCategory
             currentExpanded = prevExpanded
@@ -125,7 +126,7 @@ const NavBar = () => {
             let url = `https://localhost:7219/api/categories`
             let currentAllCategory = { ...allCategories }
             let currentCategory = null
-            
+
             if (firstCategory) {
                 url += `/first_category/${firstCategory}`
                 currentCategory = currentAllCategory[firstCategory]
@@ -138,7 +139,7 @@ const NavBar = () => {
                 url += `/third_category/${thirdCategory}`
                 currentCategory = currentCategory['subCategories'][thirdCategory]
             }
-            if (fourthCategory){
+            if (fourthCategory) {
                 setExpandedCategoriesHelper(firstCategory, secondCategory, thirdCategory, fourthCategory)
                 return
             }
@@ -224,15 +225,8 @@ const NavBar = () => {
 
                                 </div>
 
-                                {/* add folder and add file image */}
-                                <div className={classNames("nav-bar__category_img")}>
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className='nav-bar__add-folder'>
-                                        <title>create a folder</title>
-                                        <path d="M512 416c0 35.3-28.7 64-64 64L64 480c-35.3 0-64-28.7-64-64L0 96C0 60.7 28.7 32 64 32l128 0c20.1 0 39.1 9.5 51.2 25.6l19.2 25.6c6 8.1 15.5 12.8 25.6 12.8l160 0c35.3 0 64 28.7 64 64l0 256zM232 376c0 13.3 10.7 24 24 24s24-10.7 24-24l0-64 64 0c13.3 0 24-10.7 24-24s-10.7-24-24-24l-64 0 0-64c0-13.3-10.7-24-24-24s-24 10.7-24 24l0 64-64 0c-13.3 0-24 10.7-24 24s10.7 24 24 24l64 0 0 64z" /></svg>
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" className='nav-bar__add-file'>
-                                        <title>create a file</title>
-                                        <path d="M0 64C0 28.7 28.7 0 64 0L224 0l0 128c0 17.7 14.3 32 32 32l128 0 0 38.6C310.1 219.5 256 287.4 256 368c0 59.1 29.1 111.3 73.7 143.3c-3.2 .5-6.4 .7-9.7 .7L64 512c-35.3 0-64-28.7-64-64L0 64zm384 64l-128 0L256 0 384 128zm48 96a144 144 0 1 1 0 288 144 144 0 1 1 0-288zm16 80c0-8.8-7.2-16-16-16s-16 7.2-16 16l0 48-48 0c-8.8 0-16 7.2-16 16s7.2 16 16 16l48 0 0 48c0 8.8 7.2 16 16 16s16-7.2 16-16l0-48 48 0c8.8 0 16-7.2 16-16s-7.2-16-16-16l-48 0 0-48z" /></svg>
-                                </div>
+                                {/* svg for 'create a subfolder' and 'create a sub file'*/}
+                                <NavBarElementOperation />
 
                             </div>
 
@@ -283,15 +277,8 @@ const NavBar = () => {
                                                     <span>{secondCategoryName}</span>
 
                                                 </div>
-                                                {/* add folder and add image icon */}
-                                                <div className={classNames("nav-bar__category_img")}>
-                                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className='nav-bar__add-folder'>
-                                                        <title>create a folder</title>
-                                                        <path d="M512 416c0 35.3-28.7 64-64 64L64 480c-35.3 0-64-28.7-64-64L0 96C0 60.7 28.7 32 64 32l128 0c20.1 0 39.1 9.5 51.2 25.6l19.2 25.6c6 8.1 15.5 12.8 25.6 12.8l160 0c35.3 0 64 28.7 64 64l0 256zM232 376c0 13.3 10.7 24 24 24s24-10.7 24-24l0-64 64 0c13.3 0 24-10.7 24-24s-10.7-24-24-24l-64 0 0-64c0-13.3-10.7-24-24-24s-24 10.7-24 24l0 64-64 0c-13.3 0-24 10.7-24 24s10.7 24 24 24l64 0 0 64z" /></svg>
-                                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" className='nav-bar__add-file'>
-                                                        <title>create a file</title>
-                                                        <path d="M0 64C0 28.7 28.7 0 64 0L224 0l0 128c0 17.7 14.3 32 32 32l128 0 0 38.6C310.1 219.5 256 287.4 256 368c0 59.1 29.1 111.3 73.7 143.3c-3.2 .5-6.4 .7-9.7 .7L64 512c-35.3 0-64-28.7-64-64L0 64zm384 64l-128 0L256 0 384 128zm48 96a144 144 0 1 1 0 288 144 144 0 1 1 0-288zm16 80c0-8.8-7.2-16-16-16s-16 7.2-16 16l0 48-48 0c-8.8 0-16 7.2-16 16s7.2 16 16 16l48 0 0 48c0 8.8 7.2 16 16 16s16-7.2 16-16l0-48 48 0c8.8 0 16-7.2 16-16s-7.2-16-16-16l-48 0 0-48z" /></svg>
-                                                </div>
+                                                {/* svg for 'create a subfolder' and 'create a sub file'*/}
+                                                <NavBarElementOperation />
 
                                             </div>
 
@@ -341,15 +328,8 @@ const NavBar = () => {
                                                                     <span>{thirdCategoryName}</span>
 
                                                                 </div>
-                                                                {/* add folder and add image icon */}
-                                                                <div className={classNames("nav-bar__category_img")}>
-                                                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className='nav-bar__add-folder'>
-                                                                        <title>create a folder</title>
-                                                                        <path d="M512 416c0 35.3-28.7 64-64 64L64 480c-35.3 0-64-28.7-64-64L0 96C0 60.7 28.7 32 64 32l128 0c20.1 0 39.1 9.5 51.2 25.6l19.2 25.6c6 8.1 15.5 12.8 25.6 12.8l160 0c35.3 0 64 28.7 64 64l0 256zM232 376c0 13.3 10.7 24 24 24s24-10.7 24-24l0-64 64 0c13.3 0 24-10.7 24-24s-10.7-24-24-24l-64 0 0-64c0-13.3-10.7-24-24-24s-24 10.7-24 24l0 64-64 0c-13.3 0-24 10.7-24 24s10.7 24 24 24l64 0 0 64z" /></svg>
-                                                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" className='nav-bar__add-file'>
-                                                                        <title>create a file</title>
-                                                                        <path d="M0 64C0 28.7 28.7 0 64 0L224 0l0 128c0 17.7 14.3 32 32 32l128 0 0 38.6C310.1 219.5 256 287.4 256 368c0 59.1 29.1 111.3 73.7 143.3c-3.2 .5-6.4 .7-9.7 .7L64 512c-35.3 0-64-28.7-64-64L0 64zm384 64l-128 0L256 0 384 128zm48 96a144 144 0 1 1 0 288 144 144 0 1 1 0-288zm16 80c0-8.8-7.2-16-16-16s-16 7.2-16 16l0 48-48 0c-8.8 0-16 7.2-16 16s7.2 16 16 16l48 0 0 48c0 8.8 7.2 16 16 16s16-7.2 16-16l0-48 48 0c8.8 0 16-7.2 16-16s-7.2-16-16-16l-48 0 0-48z" /></svg>
-                                                                </div>
+                                                                {/* svg for 'create a subfolder' and 'create a sub file'*/}
+                                                                <NavBarElementOperation />
 
                                                             </div>
 
@@ -406,15 +386,8 @@ const NavBar = () => {
                                                                                     <span>{fourthCategoryName}</span>
 
                                                                                 </div>
-                                                                                {/* add folder and add image icon */}
-                                                                                <div className={classNames("nav-bar__category_img")}>
-                                                                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className='nav-bar__add-folder'>
-                                                                                        <title>create a folder</title>
-                                                                                        <path d="M512 416c0 35.3-28.7 64-64 64L64 480c-35.3 0-64-28.7-64-64L0 96C0 60.7 28.7 32 64 32l128 0c20.1 0 39.1 9.5 51.2 25.6l19.2 25.6c6 8.1 15.5 12.8 25.6 12.8l160 0c35.3 0 64 28.7 64 64l0 256zM232 376c0 13.3 10.7 24 24 24s24-10.7 24-24l0-64 64 0c13.3 0 24-10.7 24-24s-10.7-24-24-24l-64 0 0-64c0-13.3-10.7-24-24-24s-24 10.7-24 24l0 64-64 0c-13.3 0-24 10.7-24 24s10.7 24 24 24l64 0 0 64z" /></svg>
-                                                                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" className='nav-bar__add-file'>
-                                                                                        <title>create a file</title>
-                                                                                        <path d="M0 64C0 28.7 28.7 0 64 0L224 0l0 128c0 17.7 14.3 32 32 32l128 0 0 38.6C310.1 219.5 256 287.4 256 368c0 59.1 29.1 111.3 73.7 143.3c-3.2 .5-6.4 .7-9.7 .7L64 512c-35.3 0-64-28.7-64-64L0 64zm384 64l-128 0L256 0 384 128zm48 96a144 144 0 1 1 0 288 144 144 0 1 1 0-288zm16 80c0-8.8-7.2-16-16-16s-16 7.2-16 16l0 48-48 0c-8.8 0-16 7.2-16 16s7.2 16 16 16l48 0 0 48c0 8.8 7.2 16 16 16s16-7.2 16-16l0-48 48 0c8.8 0 16-7.2 16-16s-7.2-16-16-16l-48 0 0-48z" /></svg>
-                                                                                </div>
+                                                                                {/* svg for 'create a subfolder' and 'create a sub file'*/}
+                                                                                <NavBarElementOperation />
 
                                                                             </div>
 
