9cf93a8dad8858e64b4bf2e70d51a782ae28860c
GshunS <guoshun.su@gmail.com>
2024-08-25 01:12:16 +1200

    adjust css

--- personal_blog_frontend/src/page/BlogContent/Components/Header/Header.js
+++ personal_blog_frontend/src/page/BlogContent/Components/Header/Header.js
@@ -252,7 +252,8 @@ const Header = () => {
                             viewBox="0 0 320 512"
                             className={classNames('header__view_arrow', { arrow_expand: editable })}
                         >
-                            <path d="M137.4 374.6c12.5 12.5 32.8 12.5 45.3 0l128-128c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8L32 192c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l128 128z" /></svg>
+                            <path d="M137.4 374.6c12.5 12.5 32.8 12.5 45.3 0l128-128c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8L32 192c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l128 128z" />
+                        </svg>
 
 
                     </div>
@@ -275,10 +276,28 @@ const Header = () => {
 
                 </div>
                 {/* Theme */}
-                <div className="header__attr">Theme</div>
+                <div className="header__theme">
+                    Theme
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 320 512"
+                        className={classNames('header__theme_icon')}
+                    >
+                        <path d="M137.4 374.6c12.5 12.5 32.8 12.5 45.3 0l128-128c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8L32 192c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l128 128z" />
+                    </svg>
+                </div>
 
                 {/* Login */}
-                <div className="header__attr">Login</div>
+                <div className="header__login">
+                    Login
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 320 512"
+                        className={classNames('header__login_icon')}
+                    >
+                        <path d="M137.4 374.6c12.5 12.5 32.8 12.5 45.3 0l128-128c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8L32 192c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l128 128z" />
+                    </svg>
+                </div>
             </div>
         </div>
     );
